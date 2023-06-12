eb112e255d099b22e2f0a96c710901d517733e18
Troye <1219246435@qq.com>
2023-06-12 15:48:17 +0800

    # modified

--- 02 全栈开发/01 爬虫基础/Composition/豆瓣读书/Proxies.py
+++
@@ -1,7 +0,0 @@
-tunnel = "a130.kdltps.com:15818"
-username = "t16908862605129"
-password = "zxcv6789"
-proxies = {
-    "http": "http://%(user)s:%(pwd)s@%(proxy)s/" % {"user": username, "pwd": password, "proxy": tunnel},
-    "https": "http://%(user)s:%(pwd)s@%(proxy)s/" % {"user": username, "pwd": password, "proxy": tunnel}
-}
